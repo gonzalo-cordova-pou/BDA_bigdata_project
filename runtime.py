e5d5f3fa9cdfc2e31149467c587bc877d62add5f
gonzalo-cordova-pou <gonzalocp6@gmail.com>
2021-12-18 17:01:27 +0100

    Analysis & Runtime plantilla

--- runtime.py
+++ runtime.py
@@ -1,1 +1,22 @@
+from pyspark.mllib.tree import DecisionTree, DecisionTreeModel
+from pyspark.mllib.util import MLUtils
 
+
+sameModel = DecisionTreeModel.load(sc, "myDecisionTreeClassificationModel")
+
+
+# PARAMETERS FOR model.predict
+#  ---------------------------------------
+#    X  ->  {array-like, sparse matrix} of shape (n_samples, n_features)
+#              The input samples. Internally, it will be converted to dtype=np.float32 and if a sparse matrix is provided to a sparse csr_matrix.
+# check_input ->  bool, default=True
+#        Allow to bypass several input checking. Don’t use this parameter unless you know what you do.
+
+# Returns
+#    y  ->  array-like of shape (n_samples,) or (n_samples, n_outputs)
+#              The predicted classes, or the predict values.
+
+# Evaluate model on test instances and compute test error
+
+
+predictions = model.predict(inputData.map(lambda x: x.features))
