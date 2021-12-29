d5370268b6bef158c9772c4249b736aa828ddcc4
Miquel Palet <mikipalet@MacBook-Pro-de-Miquel.local>
2021-12-29 17:31:45 +0100

    Final version of management and utils

--- management.py
+++ management.py
@@ -1,72 +1,45 @@
-import pyspark
-import random
-import operator
-from pyspark.sql import SparkSession
-from datetime import date,timedelta
-from pyspark.mllib.util import MLUtils
-from pyspark.mllib.regression import LabeledPoint
-from tempfile import NamedTemporaryFile
-from fileinput import input
-from glob import glob
+"""
+MANAGEMENT PIPELINE
 
-g_username = "gonzalo.cordova"
-g_password = "DB060601"
-m_username = "miquel.palet.lopez"
-m_password = "DB070501"
+Steps:
+        1. Read sensor measurements from CSV files related to a certain
+           aircraft and average it per day.
+        2. Enrich with the KPIs from the Data Warehouse.
+        3. Label each row with either unscheduled maintenance or no
+           maintenance predicted in the next 7 days for that flight.
+        4. Generate a matrix with the gathered data and store it.
+
+"""
 
 def process(sc):
+
     sess = SparkSession(sc)
 
-    DW = (sess.read
-        .format("jdbc")
-        .option("driver","org.postgresql.Driver")
-        .option("url", "jdbc:postgresql://postgresfib.fib.upc.edu:6433/DW?sslmode=require")
-        .option("dbtable", "public.aircraftutilization")
-        .option("user", m_username)
-        .option("password", m_password)
-        .load())
+    KPIs = read_kpis(sess)
 
-    AMOS = (sess.read
-        .format("jdbc")
-        .option("driver","org.postgresql.Driver")
-        .option("url", "jdbc:postgresql://postgresfib.fib.upc.edu:6433/AMOS?sslmode=require")
-        .option("dbtable", "oldinstance.operationinterruption")
-        .option("user", m_username)
-        .option("password", m_password)
-        .load())
+    events  = add_rows(read_events(sess))
 
-    KPIs = (DW
-        .select("aircraftid","timeid","flighthours","flightcycles","delayedminutes")
-        .rdd
-        .map(lambda t: ((t[1],t[0]),(float(t[2]),int(t[3]),int(t[4]))))
-        .sortByKey())
+    # ------------- 1. READ CSV FILES -------------
 
-    OI  = (AMOS
-        .select("aircraftregistration","starttime","kind", "subsystem")
-        .rdd
-        .map(lambda t: ((t[1].date(),t[0]),(t[2],t[3])))
-        .filter(lambda t: t[1][0] in ["Delay","AircraftOnGround","Safety"])
-		.filter(lambda t: t[1][1] == '3453')
-		.mapValues(lambda t: t[0])
-        .sortByKey())
+    CSVfiles = extract_csv("./resources/trainingData/*.csv")
 
-    input = (sc.wholeTextFiles("./resources/trainingData/*.csv")
-        .map(lambda t: ((date(2000+int(t[0].split("/")[-1][4:6]),int(t[0].split("/")[-1][2:4]),int(t[0].split("/")[-1][0:2])),t[0].split("/")[-1][20:26]),list(t[1].split("\n"))))
-        .flatMap(lambda t: [(t[0], value) for value in t[1]])
-        .filter(lambda t: "value" not in t[1])
-        .filter(lambda t: t[1] != '')
-        .mapValues(lambda t: (float(t.split(";")[-1]),1))
-        .reduceByKey(lambda t1,t2: (t1[0]+t2[0],t1[1]+t2[1]))
-        .mapValues(lambda t: t[0]/t[1])
-        .sortByKey())
-    
-    print(input.collect())
 
-    out = (input
+    output = (CSVfiles
+
+    # ------------- 2. ENRICH WITH KPIs -------------
+
         .join(KPIs)
-        .leftOuterJoin(OI
-            .flatMap(lambda t: [((t[0][0] - timedelta(days=days),t[0][1]),t[1]) for days in range(8)]))
-		.mapValues(lambda t: (t[0], t[1] is not None))
-        .map(lambda t: LabeledPoint(t[1][1],[t[1][0][0],t[1][0][1][0],t[1][0][1][1],t[1][0][1][2]])))
 
-    MLUtils.saveAsLibSVMFile(out, "./LibSVM-files/")
+    # ------------- 3. LABEL ROWS -------------
+
+        .leftOuterJoin(events)
+        # Format labels as binary (0 for no maintenance, 1 for maintenance)
+        .mapValues(lambda t: (t[0], t[1] is not None))
+
+    # ------------- 4. FORMAT AND STORE DATA -------------
+
+        # Format data with LibSVM format (label index1:value1 index2:value2 ...)
+        .map(lambda t: LabeledPoint(t[1][1],[t[1][0][0],t[1][0][1][0],
+                                             t[1][0][1][1],t[1][0][1][2]])))
+
+    MLUtils.saveAsLibSVMFile(output, "./LibSVM-files/")
--- utils.py
+++ utils.py
@@ -48,4 +48,90 @@ def get_files(aircraft, date_):
     
     return target_files
 
+
+def read_aircraftutilization(session):
+    """
+    Returns aircraft utilization table from the Data Warehouse.
+    """
+
+    return (session.read
+        .format("jdbc")
+        .option("driver","org.postgresql.Driver")
+        .option("url", "jdbc:postgresql://postgresfib.fib.upc.edu:6433/DW?sslmode=require")
+        .option("dbtable", "public.aircraftutilization")
+        .option("user", m_username)
+        .option("password", m_password)
+        .load())
+
+def read_kpis(session):
+    """
+    Returns FH, FC and DM KPIs for aircraft and day as RDD.
+    """
+
+    return (read_aircraftutilization(session)
+        .select("aircraftid","timeid","flighthours","flightcycles","delayedminutes")
+        .rdd
+        .map(lambda t: ((t[1],t[0]),(float(t[2]),int(t[3]),int(t[4]))))
+        .sortByKey())
+
+def read_operationinterruption(session):
+    """
+    Returns operation inerruption table from the AMOS database.
+    """
+
+    return (sess.read
+        .format("jdbc")
+        .option("driver","org.postgresql.Driver")
+        .option("url", "jdbc:postgresql://postgresfib.fib.upc.edu:6433/AMOS?sslmode=require")
+        .option("dbtable", "oldinstance.operationinterruption")
+        .option("user", m_username)
+        .option("password", m_password)
+        .load())
+
+def read_events(session):
+    """
+    Returns unscheduled maintenance events for aircraft and day logged by
+    the aircraft navigation subsystem as RDD.
+    """
+
+    return = (read_operationinterruption(session)
+    .select("aircraftregistration","starttime","kind","subsystem")
+    .rdd
+    .map(lambda t: ((t[1].date(),t[0]),(t[2],t[3])))
+    .filter(lambda t: t[1][0] in ["Delay","AircraftOnGround","Safety"])
+	.filter(lambda t: t[1][1] == '3453')
+	.mapValues(lambda t: t[0])
+    .sortByKey())
+
+def extract_csv(path):
+    """
+    Returns average sensor measurements for aircraft and day as RDD.
+    """
+
+    return (sc.wholeTextFiles(path)
+        .map(lambda t: ((date(2000+int(t[0].split("/")[-1][4:6]), # year
+                              int(t[0].split("/")[-1][2:4]),      # month
+                              int(t[0].split("/")[-1][0:2])),     # day
+                         t[0].split("/")[-1][20:26]),             # aircraft id
+                        list(t[1].split("\n"))))
+        # Separate rows for each measurement
+        .flatMap(lambda t: [(t[0], value) for value in t[1]])
+        .filter(lambda t: "value" not in t[1])                     # | data
+        .filter(lambda t: t[1] != '')                              # | cleaning
+        .mapValues(lambda t: (float(t.split(";")[-1]),1))          # |
+        # Compute average measurement
+        .reduceByKey(lambda t1,t2: (t1[0]+t2[0],t1[1]+t2[1]))
+        .mapValues(lambda t: t[0]/t[1])
+        .sortByKey())
+
+def add_rows(df):
+    """
+    Generates and returns seven extra rows substracting
+    1,2,...7 days from every given row.
+    """
+
+    return (df
+        .flatMap(lambda t: [((t[0][0] - timedelta(days=days),t[0][1]),t[1])
+                            for days in range(8)]))
+
 #---------------------------------------------#
